101162537cdbc7740abf3e20a999bf3e9743ac1f
Regidan-codes <abhinadiger@gmail.com>
2023-11-25 20:59:07 +0530

    added links, domains, urls and removed hardcoded topic

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "e5959d92dac14f1da70f50b3255ef416"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-10-25&sortBy=publishedAt&apiKey"
-       "=e5959d92dac14f1da70f50b3255ef416")
+url = ("https://newsapi.org/v2/everything?"
+       "domains=techcrunch.com,thenextweb.com,inverse.com&"
+       "from=2023-10-25&"
+       "sortBy=popularity&"
+       "apiKey=e5959d92dac14f1da70f50b3255ef416&"
+       "language=en")
 
 # Make request
 response = requests.get(url)
@@ -13,9 +18,9 @@ content = response.json()
 
 # Iterate through a dictionary
 body = ""
-for item in content['articles']:
-    if item['title'] is not None:
-        body += item['title'] + "\n" + item['description'] + 2*"\n"
+for item in content['articles'][:20]:
+    if item['title'] and item['description'] is not None:
+        body += item['title'] + "\n" + item['description'] + "\n" + item['url'] + 2 * "\n"
 
 body = body.encode('utf-8')
 send_email(message=body)
