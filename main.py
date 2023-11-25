86bd752b51a9a625e9108f993a74c3b209988510
Regidan-codes <abhinadiger@gmail.com>
2023-11-25 17:42:03 +0530

    sent to email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "e5959d92dac14f1da70f50b3255ef416"
 url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-10-25&sortBy=publishedAt&apiKey"
@@ -11,8 +12,10 @@ response = requests.get(url)
 content = response.json()
 
 # Iterate through a dictionary
+body = ""
 for item in content['articles']:
-    print(item['title'])
-    print(item['description'])
+    if item['title'] is not None:
+        body += item['title'] + "\n" + item['description'] + 2*"\n"
 
-
+body = body.encode('utf-8')
+send_email(message=body)
